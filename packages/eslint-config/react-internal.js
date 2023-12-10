bc8e8c46c61b7f119c28f926fa01a5e720c7b073
Haku <hakudevtw@gmail.com>
2023-12-10 13:03:41 +0800

    feat: add basic structure

--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -14,7 +14,12 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
+  extends: [
+    "eslint:recommended",
+    "plugin:tailwindcss/recommended",
+    "prettier",
+    "eslint-config-turbo",
+  ],
   plugins: ["only-warn"],
   globals: {
     React: true,
