3c95b6b017c26055b1ab1f44f87058724dd249c2
Haku <hakudevtw@gmail.com>
2024-02-03 14:36:53 +0800

    chore: update environment

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,1 +1,11 @@
-module.exports = require("@repo/ui/tailwind.config");
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/src/**/*.{ts,tsx}",
+  ],
+  
+  presets: [require("@repo/ui/tailwind.config")],
+};
--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "../../packages/ui/lib/components/**/*.{ts,tsx}",
-  ],
   theme: {
     container: {
       center: true,
